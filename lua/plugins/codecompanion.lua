5aa8ee4b8fb0f87ac263e82f2396203baf81cada
Roman Chesnakov <romus204@gmail.com>
2025-07-16 02:12:19 +0700

    refactor codecompanion config

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -1,24 +1,26 @@
+local adapters = require("codecompanion.adapters")
+
+local gemini_adapter = function()
+    return adapters.extend("gemini", {
+        schema = {
+            model = { default = "gemini-2.5-flash", },
+        },
+        env = { api_key = os.getenv("GEMINI_API_KEY"), },
+    })
+end
+
+local ollama_adapter = function()
+    return adapters.extend("ollama", {
+        schema = {
+            model = { default = "qwen2.5-coder:14b" },
+        },
+    })
+end
+
 require("codecompanion").setup({
     adapters = {
-        gemini = function()
-            return require("codecompanion.adapters").extend("gemini", {
-                schema = {
-                    model = {
-                        default = "gemini-2.5-pro",
-                    },
-                },
-                env = {
-                    api_key = os.getenv("GEMINI_API_KEY"),
-                },
-            })
-        end,
-        ollama = function()
-            return require("codecompanion.adapters").extend("ollama", {
-                schema = {
-                    model = { default = "qwen2.5-coder:14b" },
-                },
-            })
-        end,
+        gemini = gemini_adapter,
+        ollama = ollama_adapter,
     },
     strategies = {
         chat = { adapter = "ollama" },
