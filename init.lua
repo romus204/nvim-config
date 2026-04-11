2b8ce48dbce76e1287d05bd524f4f190efcaea4d
romarik <romus204@gmail.com>
2026-04-11 23:44:39 +0700

    move generete uuid

--- init.lua
+++ init.lua
@@ -5,7 +5,6 @@ require("core.configs")
 require("core.mappings")
 require("core.lsp")
 -- Toolkit (is a other useful functions)
-require('toolkit.generate')
 require('toolkit.files_dap')
 require('toolkit.env')
 -- Plugins config
