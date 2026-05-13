c01368e3da42c22f17d5bc5a6c9b1aa69c34cafa
romarik <romus204@gmail.com>
2026-05-14 02:05:45 +0700

    delete gitlab plugin and modify gopls settings

--- lua/core/lsp.lua
+++ lua/core/lsp.lua
@@ -2,6 +2,21 @@ vim.pack.add({
     "https://github.com/neovim/nvim-lspconfig",
 })
 
+vim.lsp.config('gopls', {
+    settings = {
+        gopls = {
+            buildFlags = { "-tags=integration" },
+            staticcheck = true,
+            usePlaceholders = true,
+            symbolMatcher = "fuzzy",
+            analyses = {
+                shadow = true,
+                slicesdelete = true,
+            }
+        },
+    },
+})
+
 vim.lsp.enable({
     'gopls',
     'lua_ls',
