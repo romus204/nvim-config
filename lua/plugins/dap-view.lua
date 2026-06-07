6a8bafa03f1ed9bddbba627ffc8ad7f7d76847a3
romarik <romus204@gmail.com>
2026-06-07 14:07:11 +0700

    edit nvim-dap-view settings

--- lua/plugins/dap-view.lua
+++ lua/plugins/dap-view.lua
@@ -6,7 +6,7 @@ vim.pack.add({
 
 require("dap-view").setup({
     winbar = {
-        sections = { "scopes", "breakpoints", "threads", "repl" },
+        sections = { "scopes", "breakpoints", "threads", "repl", "sessions" },
         default_section = "repl",
     },
     virtual_text = {
