5c09f777a3094324236b915a89d58b9f6c597b49
romarik <romus204@gmail.com>
2026-06-07 14:06:44 +0700

    add gitlab plugin

---
+++ lua/plugins/gitlab.lua
@@ -0,0 +1,12 @@
+vim.pack.add({
+    "https://github.com/MunifTanjim/nui.nvim",
+    "https://github.com/nvim-lua/plenary.nvim",
+    "https://github.com/dlyongemallo/diffview.nvim",  -- Maintained fork of "sindrets/diffview.nvim".
+    "https://github.com/stevearc/dressing.nvim",      -- Recommended but not required. Better UI for pickers.
+    "https://github.com/nvim-tree/nvim-web-devicons", -- Recommended but not required. Icons in discussion tree.
+
+    "https://github.com/harrisoncramer/gitlab.nvim",
+})
+
+-- require("gitlab.server").build(true)
+require("gitlab").setup()
